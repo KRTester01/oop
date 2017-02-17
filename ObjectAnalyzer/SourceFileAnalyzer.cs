4936afa6f960aa9a8e4df58194b5258c98847eca
kaytlen <kaytlen>
2017-02-16 21:06:53 -0500

    First commit

---
+++ ObjectAnalyzer/SourceFileAnalyzer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectAnalyzer
+{
+    class SourceFileAnalyzer
+    {
+        public string GetTheMethodDetails
+
+            return "";
+    }
+}
---
+++ ObjectAnalyzer/fileReader.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectAnalyzer
+{
+    class fileReader
+    {
+        public fileReader();
+
+
+
+        public string ReadInTheFile()
+            String code = System.IO.File.ReadAllText(c: \\projects\\temp\\objectAnalyzer\\objextAnalyzer\\Program.cs)
+            return code;
+    }
+}
